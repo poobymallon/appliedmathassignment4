e4cb6f85757b402b67d04a2a7140a5b19dfcb811
poobymallon <157262682+poobymallon@users.noreply.github.com>
2025-10-24 14:18:29 -0400

    finish embedded

--- coopervariable.m
+++ coopervariable.m
@@ -1,35 +1,11 @@
 function coopervariable()
-    m_sun = 1;
-    m_planet = 1/330000;
-    G = 4*pi^2/m_sun;
+    orbit_params = struct();
+    orbit_params.m_sun = 1;
+    orbit_params.m_planet = 1/330000;
+    orbit_params.G = 4*pi^2/orbit_params.m_sun;
     ti = 0;
     tf = 10;
     V0 = [1; 0; 0; 6.28;];
-
-    %Heun's method (s = 2)
-    Heun_struct.A = [0, 0; 1, 0;];
-    Heun_struct.B = [0.5, 0.5;];
-    Heun_struct.C = [0; 1;];
-    Heun_struct.m_sun = m_sun;
-    Heun_struct.m_planet = m_planet;
-    Heun_struct.G = G;
-
-
-    %Ralston's third order method (s = 3)
-    Ral_struct.A = [0,0,0;0.5,0,0;0,0.75,0;];
-    Ral_struct.B = [2/9;1/3;4/9;];
-    Ral_struct.C = [0;0.5;0.75;];
-    Ral_struct.m_sun = m_sun;
-    Ral_struct.m_planet = m_planet;
-    Ral_struct.G = G;
-
-    %og Runge-Kutta (s = 4)
-    og_struct.A = [0,0,0,0;1/2,0,0,0;0,1/2,0,0;0,0,1,0;];
-    og_struct.B = [1/6,1/3,1/3,1/6;];
-    og_struct.C = [0;1/2;1/2;1;];
-    og_struct.m_sun = m_sun;
-    og_struct.m_planet = m_planet;
-    og_struct.G = G;
 
     DormandPrince = struct();
     DormandPrince.C = [0, 1/5, 3/10, 4/5, 8/9, 1, 1];
@@ -42,31 +18,71 @@ function coopervariable()
     19372/6561, -25360/2187, 64448/6561, -212/729, 0, 0,0;...
     9017/3168, -355/33, 46732/5247, 49/176, -5103/18656, 0,0;...
     35/384, 0, 500/1113, 125/192, -2187/6784, 11/84,0];
+    dorp = 4;
 
     tspan = [ti,tf];
+    grav_wrapper = @(t, V) gravity_rate_func(t, V, orbit_params);
 
-
-    %in class local truncation figure
-    n_samples = 60;
-    h_ref_list = logspace(-3, 1, n_samples)
-
-    abs_diff_list = zeros(1, n_samples);
-
-    for i = 1:length(h_ref_list);
-        h_ref = h_ref_list(i);
-        V_list = compute_planetary_motion(ti+h_ref,V0,orbit_params);
-
-        [XB1, XB2, ~] = explicit_RK_step_embedded(my_rate, ti, V0, h_ref, DormandPrince);
-
-        abs_diff_list(i) = norm(V_list-V0)
-        tr_error_list1(i) = norm(XB1-V_list);
-        tr_error_list2(i) = norm(XB1-V_list);
-    end
+    %test working
+    des_err = 1e-8;
+    [t_list,X_list,~, ~] = explicit_RK_variable_step_integration(grav_wrapper, tspan, V0, 1/36.5, DormandPrince, 5, des_err);
+    tforV = linspace(ti,tf, 366);
+    V_list = compute_planetary_motion(tforV, V0, orbit_params);
 
     figure
-    loglog(h_ref_list, abs_diff_list)
-    loglog(h_ref_list, tr_error_list1)
-    loglog(h_ref_list, tr_error_list2)
+    subplot(2,1,1)
+    plot(t_list, X_list(1,:), "r--", "DisplayName", "Runge-Kutta")
+    legend()
+    title('x position over time - runge-kutta')
+    subplot(2,1,2)
+    plot(tforV, V_list(:,1), 'b', "DisplayName", "Secant Method")
+    legend()
+    title('x position over time - secant')
+
+    figure
+    subplot(2,1,1)
+    plot(X_list(1,:), X_list(2,:), "r--", "DisplayName", "Runge-Kutta")
+    legend()
+    title('y vs x position of earth - runge-kutta')
+    subplot(2,1,2)
+    plot(V_list(:,1), V_list(:,2), 'b', "DisplayName", "Secant Method")
+    legend()
+    title('y vs x position of earth - secant')
+
+
+    figure
+    hold on
+    plot(X_list(1,:), X_list(2,:), "r--", "DisplayName", "Runge-Kutta")
+    plot(V_list(:,1), V_list(:,2), 'b', "DisplayName", "Secant Method")
+    legend()
+    title('y vs x position of earth')
+
+    h_ref_list = logspace(-5,-1, 30);
+    num_evals_list = [];
+    h_avg_list = [];
+    tr_error_list = [];
+
+    % %in class local truncation figure
+    % n_samples = 60;
+    % h_ref_list = logspace(-3, 1, n_samples)
+    % 
+    % abs_diff_list = zeros(1, n_samples);
+    % 
+    % for i = 1:length(h_ref_list)
+    %     h_ref = h_ref_list(i);
+    %     V_list = compute_planetary_motion(ti+h_ref,V0,orbit_params);
+    % 
+    %     [XB1, ~, ~] = RK_step_embedded(grav_wrapper, ti, V0, h_ref, DormandPrince);
+    % 
+    %     abs_diff_list(i) = norm(V_list-V0);
+    %     tr_error_list1(i) = norm(XB1-V_list);
+    %     tr_error_list2(i) = norm(XB1-V_list);
+    % end
+    % 
+    % figure
+    % loglog(h_ref_list, abs_diff_list)
+    % loglog(h_ref_list, tr_error_list1)
+    % loglog(h_ref_list, tr_error_list2)
 
 
 
@@ -81,7 +97,7 @@ function [XB1, XB2, num_evals] = RK_step_embedded(rate_func_in,t,XA,h,BT_struct)
     ks = zeros(m, s);
     for i = 1:s
         SUMak = ks*(as(i,:)');
-        ki = rate_func_in((t+cs(i)*h), (XA+h*SUMak), BT_struct);
+        ki = rate_func_in((t+cs(i)*h), (XA+h*SUMak));
         ks(:, i) = ki;
     end
     SUMbk1 = ks*bs(1,:)';
@@ -96,30 +112,36 @@ function [XB, num_evals, h_next, redo] = explicit_RK_variable_step(rate_func_in,
 
     redo = false;
     [XB1, XB2, num_evals] = RK_step_embedded(rate_func_in,t,XA,h,BT_struct);
-    epsc = abs(XB1-XB2);
-    temp = (error_desired/epsc)^(1/p);
+    epsc = norm(XB1-XB2);
+    temp = (error_desired/epsc).^(1/p);
     h_next = min(0.9*temp, alpha)*h;
-    
+    XB = XB1;
     if error_desired < epsc
         redo = true;
     end
 end
 
-function [t_list, X_list, h_avg, num_evals] = fixed_step_integration(rate_func_in, step_func, tspan, X0, h_ref, BT_struct)
+function [t_list, X_list, h_avg, num_evals] = variable_step_integration(rate_func_in, step_func, tspan, X0, h_ref, BT_struct, p, error_desired)
     ti = tspan(1); tf = tspan(2);
-    N  = ceil((tf - ti)/h_ref); h_avg = (tf - ti)/N;
+    N  = ceil((tf - ti)/h_ref); h = (tf - ti)/N;
     t_list = linspace(ti, tf, N+1);
     nx = numel(X0); X_list = zeros(nx, N+1); X_list(:,1) = X0;
     num_evals = 0; XA = X0;
+    h_rec = []; 
     for k = 1:N
-        [XB, add_evals] = step_func(rate_func_in, t_list(k), XA, h, BT_struct, p, error_desired);
-        num_evals = num_evals + add_evals;
+        redo = true;
+        while redo == true
+            h_rec(end+1,:) = h;
+            [XB, add_evals, h, redo] = step_func(rate_func_in, t_list(k), XA, h, BT_struct, p, error_desired);
+            num_evals = num_evals + add_evals;
+        end
         X_list(:,k+1) = XB; XA = XB;
     end
+    h_avg = mean(h_rec);
 end
 
 function [t_list,X_list,h_avg, num_evals] = explicit_RK_variable_step_integration(rate_func_in,tspan,X0,h_ref,BT_struct, p, error_desired)
-    [t_list,X_list,h_avg, num_evals] = fixed_step_integration(rate_func_in,@RK_step_embedded,tspan,X0,h_ref,BT_struct, p, error_desired);
+    [t_list,X_list,h_avg, num_evals] = variable_step_integration(rate_func_in, @explicit_RK_variable_step, tspan, X0, h_ref, BT_struct, p, error_desired);
 end
 
 function dVdt = gravity_rate_func(t,V,orbit_params)
